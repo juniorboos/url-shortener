236c3aff3436c47d2ebd1f5d5ab7c91d110a2cf8
Milton Boos Junior <junior_boos@live.com>
2020-12-17 21:31:56 +0000

    initial config

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main>Hello World</main>
+    </div>
+  )
+}
+
+export default Home
