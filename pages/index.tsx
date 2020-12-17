e97b541e8a79a8ec34dda9bb0b929fe62475b298
Milton Boos Junior <junior_boos@live.com>
2020-12-17 19:57:01 +0000

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+   return (
+      <div>
+         <Head>
+            <title>Homepage</title>
+         </Head>
+
+         <main>Hello World</main>
+      </div>
+   );
+}
