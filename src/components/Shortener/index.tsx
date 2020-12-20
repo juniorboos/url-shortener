37bacba19a27d3c79da018f36bdf9654de0b205c
Milton Boos Junior <junior_boos@live.com>
2020-12-20 14:59:46 +0000

    added shorten function

--- src/components/Shortener/index.tsx
+++ src/components/Shortener/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
+/* eslint-disable camelcase */
+import React, { FormEvent, useState } from 'react'
 import { Container } from '../../styles/components/shortener'
+import axios from 'axios'
 
 const Shortener: React.FC = () => {
+  const [url, setUrl] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const shorten = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setLoading(true)
+    try {
+      axios
+        .post(`https://api.shrtco.de/v2/shorten?url=${url}`)
+        .then(response => {
+          console.log(response.data.result.short_link)
+        })
+      setLoading(false)
+      setUrl('')
+    } catch (err) {
+      console.log(err)
+      setLoading(false)
+    }
+  }
   return (
     <Container>
-      <form action="submit">
-        <input type="text" placeholder="Shorten a link here..." />
-        <button>Shorten It!</button>
+      <form onSubmit={shorten}>
+        <input
+          type="text"
+          placeholder="Shorten a link here..."
+          value={url}
+          onChange={e => setUrl(e.target.value)}
+        />
+        <button type="submit" disabled={loading}>
+          Shorten It!
+        </button>
       </form>
     </Container>
   )
